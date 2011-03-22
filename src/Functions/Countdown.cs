e2ba9c30fd09a891ebcb36461921c4969aa2e9f8
Tyranic-Moron <tyranicmoron@gmail.com>
2011-03-22 00:50:31 +0000

    Starting work on Countdown function, will allow you to set up events and retrieve the time until they occur.
    Will probably need a CountdownAuto to announce things, Event to set up events, Countdown/Time(Un)Till to get the current time gap, and a way of saving and loading events.
    
    Branched because BRANCHES WOO :D
    Also the rest of MoronBot is pretty complete right now, so, makes sense to branch for new un-finished functions I think.

---
+++ src/Functions/Countdown.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+
+using CwIRC;
+
+namespace MoronBot.Functions
+{
+    class Countdown : Function
+    {
+        public struct TellMessage
+        {
+            public string From;
+            public string Message;
+        }
+
+        struct UserDateNumber
+        {
+            public string User { get; set; }
+            public DateTime MessageTime { get; set; }
+            public uint Number { get; set; }
+        }
+
+        public static Dictionary<string, List<TellMessage>> MessageMap = new Dictionary<string, List<TellMessage>>();
+
+        List<UserDateNumber> userDateNumberList = new List<UserDateNumber>();
+
+        public Countdown(MoronBot moronBot)
+        {
+            Name = GetName();
+            Help = "countdown/time(un)till <event>\t\t- Tells you the amount of time left until the specified event.";
+            Type = Types.Command;
+            AccessLevel = AccessLevels.Anyone;
+        }
+        
+        public override IRCResponse GetResponse(BotMessage message, MoronBot moronBot)
+        {
+            if (Regex.IsMatch(message.Command, "^(countdown|time(un)?till)$", RegexOptions.IgnoreCase))
+            {
+                if (message.Parameters.Length > 0)
+                {
+                    return new IRCResponse(ResponseType.Say, message.Parameters, message.ReplyTo);
+                }
+                else
+                {
+                    return new IRCResponse(ResponseType.Say, "Say what?", message.ReplyTo);
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
