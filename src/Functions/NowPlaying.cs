22b357911917099dfb14a7c54bbdc84626464be0
Tyranic-Moron <tyranicmoron@gmail.com>
2011-03-26 17:14:41 +0000

    Added NowPlayingRegister, allowing people to link their IRC name to their Last.fm acount name, if they want to.
    NowPlaying will now fetch Last.fm names from a dictionary, if the user's name is a key in the dictionary.

--- src/Functions/NowPlaying.cs
+++ src/Functions/NowPlaying.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 using CwIRC;
 using Bitly;
@@ -7,17 +8,19 @@ namespace MoronBot.Functions
 {
     class NowPlaying : Function
     {
+        public static Dictionary<string, string> AccountMap = new Dictionary<string, string>();
+
         public NowPlaying(MoronBot moronBot)
         {
             Name = GetName();
-            Help = "nowplaying/np (<user>)\t\t- Returns your currently playing music (from Last.fm). You can also supply a specific username to check.";
+            Help = "np (<user>)\t\t- Returns your currently playing music (from Last.fm). You can also supply a specific username to check.";
             Type = Types.Command;
             AccessLevel = AccessLevels.Anyone;
         }
 
         public override void GetResponse(BotMessage message, MoronBot moronBot)
         {
-            if (Regex.IsMatch(message.Command, "^(n(ow)?p(laying)?)$", RegexOptions.IgnoreCase))
+            if (Regex.IsMatch(message.Command, "^(np|nowplaying)$", RegexOptions.IgnoreCase))
             {
                 string lastfmName = "";
 
@@ -27,7 +30,14 @@ namespace MoronBot.Functions
                 }
                 else
                 {
-                    lastfmName = message.User.Name;
+                    if (AccountMap.ContainsKey(message.User.Name.ToUpper()))
+                    {
+                        lastfmName = AccountMap[message.User.Name.ToUpper()];
+                    }
+                    else
+                    {
+                        lastfmName = message.User.Name;
+                    }
                 }
 
                 Utilities.URL.WebPage recentFeed = new Utilities.URL.WebPage();
@@ -50,6 +60,46 @@ namespace MoronBot.Functions
 
                 moronBot.MessageQueue.Add(new IRCResponse(ResponseType.Say, "\"" + song.Trim() + "\" by " + band.Trim() + " (" + link + ")", message.ReplyTo));
                 return;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    class NowPlayingRegister : Function
+    {
+        public NowPlayingRegister(MoronBot moronBot)
+        {
+            Name = GetName();
+            Help = "npregister/nplink <Last.fm Name>\t\t- Links the specified Last.fm account name to your IRC name.";
+            Type = Types.Command;
+            AccessLevel = AccessLevels.Anyone;
+        }
+
+        public override void GetResponse(BotMessage message, MoronBot moronBot)
+        {
+            if (Regex.IsMatch(message.Command, "^(np(register|link))$", RegexOptions.IgnoreCase))
+            {
+                if (message.ParameterList.Count > 0)
+                {
+                    if (NowPlaying.AccountMap.ContainsKey(message.User.Name.ToUpper()))
+                    {
+                        NowPlaying.AccountMap[message.User.Name.ToUpper()] = message.ParameterList[0];
+                    }
+                    else
+                    {
+                        NowPlaying.AccountMap.Add(message.User.Name.ToUpper(), message.ParameterList[0]);
+                    }
+                    moronBot.MessageQueue.Add(new IRCResponse(ResponseType.Say, "Last.fm account \"" + message.ParameterList[0] + "\" is now linked to IRC name \"" + message.User.Name + "\""));
+                    return;
+                }
+                else
+                {
+                    moronBot.MessageQueue.Add(new IRCResponse(ResponseType.Say, "You didn't specify a Last.fm account name! Format is \"" + message.Command + " <Last.fm Account>\"", message.ReplyTo));
+                    return;
+                }
             }
             else
             {
