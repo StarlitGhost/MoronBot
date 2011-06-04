6d8d44f5d1f85ecb8534c5b718eee105394af6f6
Tyranic-Moron <tyranicmoron@gmail.com>
2011-06-04 20:24:02 +0100

    Created SentTells function. Closes #20

---
+++ MoronBot-Functions/Tell/SentTells.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using CwIRC;
+using MBFunctionInterface;
+using MBUtilities;
+
+namespace Utility
+{
+    public class SentTells : Function
+    {
+        public SentTells()
+        {
+            Help = "s(ent)tells - Tells you all of the messages you have sent that have yet to be received.";
+            Type = Types.Command;
+            AccessLevel = AccessLevels.Anyone;
+        }
+
+        public override List<IRCResponse> GetResponse(BotMessage message)
+        {
+            if (Regex.IsMatch(message.Command, "^((outstanding|s(ent)?)tells)$", RegexOptions.IgnoreCase))
+            {
+                List<IRCResponse> responses = new List<IRCResponse>();
+
+                foreach (KeyValuePair<string, List<Tell.TellMessage>> kvp in Tell.MessageMap)
+                {
+                    bool fromUser = false;
+                    foreach (Tell.TellMessage msg in kvp.Value)
+                    {
+                        if (msg.From == message.User.Name)
+                        {
+                            if (!fromUser)
+                            {
+                                fromUser = true;
+                                responses.Add(new IRCResponse(ResponseType.Say, "To " + kvp.Key, message.ReplyTo));
+                            }
+                            responses.Add(new IRCResponse(ResponseType.Say, " > " + msg.Message, message.ReplyTo));
+                        }
+                    }
+                }
+
+                if (responses.Count == 0)
+                {
+                    responses.Add(new IRCResponse(ResponseType.Say, "There are no messages from you that have not already been received.", message.ReplyTo));
+                }
+                
+                return responses;
+            }
+
+            return null;
+        }
+    }
+}
