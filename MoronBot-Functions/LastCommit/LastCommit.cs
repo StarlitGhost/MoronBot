d69a8144ffbc07f4135f9dc4420a80fb94170bf6
Matthew Cox <matthewcpcox@gmail.com>
2011-09-01 16:30:26 +0100

    Ok, *now* LastCommit works correctly with multiline commits.
    Stupid Regex being all confusing ¬_¬ (although, the final solution turned out to be pretty simple)

--- MoronBot-Functions/LastCommit/LastCommit.cs
+++ MoronBot-Functions/LastCommit/LastCommit.cs
@@ -27,9 +27,9 @@ namespace GitHub
 
                 if (commitFeed.Page != null)
                 {
-                    MatchCollection commitMessages = Regex.Matches(commitFeed.Page, @"width:81ex'>(.+)?\&lt;/pre>", RegexOptions.IgnoreCase | RegexOptions.Singleline);
-
-                    return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "Last Commit Message: " + commitMessages[0].Groups[1].Value.Replace("\n", " | "), message.ReplyTo) };
+                    MatchCollection commitMessages = Regex.Matches(commitFeed.Page, @"width:81ex'>([^<]+?)\&lt;/pre>", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                    string lastMessage = Regex.Replace(commitMessages[0].Groups[1].Value, @"\n+", " | ");
+                    return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "Last Commit Message: " + lastMessage, message.ReplyTo) };
                 }
                 else
                 {
