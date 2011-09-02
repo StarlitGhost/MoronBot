c99d0650f85096c3bed462d6d939dd251bb1a123
Matthew Cox <matthewcpcox@gmail.com>
2011-09-02 03:55:45 +0100

    Created a project for Hangman (#4)

---
+++ MoronBot-Functions/Hangman/Hangman.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using CwIRC;
+using MBFunctionInterface;
+
+namespace Fun
+{
+    public class Hangman : Function
+    {
+        public Hangman()
+        {
+            Help = "h(ang)m(an) <command> (<params>) - A game of hangman. Sub-commands are as follows: start, stop, guess";
+            Type = Types.Command;
+            AccessLevel = AccessLevels.Anyone;
+        }
+
+        public override List<IRCResponse> GetResponse(BotMessage message)
+        {
+            if (Regex.IsMatch(message.Command, "^(h(ang)?m(an)?)$", RegexOptions.IgnoreCase))
+            {
+
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
