14294e2319af7daa5817d70d66f21c2848a9b1e8
Matthew Cox <matthewcpcox@gmail.com>
2011-09-05 21:42:55 +0100

    Got Hangman player statistics stored in a database! This is progress on #4 and #7
    Also got a shiny new SQLiteInterface, wrapped around c#-sqlite.

--- MoronBot-Functions/Hangman/Hangman.cs
+++ MoronBot-Functions/Hangman/Hangman.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 
+using Community.CsharpSqlite.SQLiteClient;
+
 using CwIRC;
 using MBFunctionInterface;
 using MBUtilities;
@@ -22,7 +24,7 @@ namespace Fun
 
         public Hangman()
         {
-            Help = "h(ang)m(an) <command> (<params>) - A game of hangman. Sub-commands are as follows: start, stop, max, <guess>";
+            Help = "h(ang)m(an) <command> (<params>) - A game of hangman. Sub-commands are as follows: start, stop, max, score (<user>), <guess>";
             Type = Types.Command;
             AccessLevel = AccessLevels.Anyone;
 
@@ -47,15 +49,22 @@ namespace Fun
                     case "start":
                         if (!playing)
                         {
-                            responses.Add(new IRCResponse(ResponseType.Say, Start(), message.ReplyTo));
-                            responses.Add(new IRCResponse(ResponseType.Say, VisibleWord(word, guesses), message.ReplyTo));
+                            Start();
+                            responses.Add(new IRCResponse(ResponseType.Say, "Hangman game started!", message.ReplyTo));
+                            responses.Add(new IRCResponse(
+                                ResponseType.Say,
+                                ProgressString(),
+                                message.ReplyTo));
                         }
                         else
                             responses.Add(new IRCResponse(ResponseType.Say, "There is already a game of Hangman in progress!", message.ReplyTo));
                         break;
                     case "stop":
                         if (ChannelList.UserIsAnyOp(message.User.Name, message.ReplyTo))
-                            responses.Add(new IRCResponse(ResponseType.Say, Stop(), message.ReplyTo));
+                        {
+                            Stop();
+                            responses.Add(new IRCResponse(ResponseType.Say, "Hangman game stopped!", message.ReplyTo));
+                        }
                         else
                             responses.Add(new IRCResponse(ResponseType.Say, "You need to be an operator to stop a game of Hangman!", message.ReplyTo));
                         break;
@@ -66,27 +75,81 @@ namespace Fun
                             if (message.ParameterList.Count > 1 && Int32.TryParse(message.ParameterList[1], out temp))
                                 maxBadGuesses = (temp < 50 ? (temp >= 0 ? temp : 0) : 50);
                             else
-                                responses.Add(new IRCResponse(ResponseType.Say, "You didn't give a number!", message.ReplyTo));
+                                responses.Add(new IRCResponse(ResponseType.Say, "You didn't give a number! |hm max <0-50>", message.ReplyTo));
 
                             responses.Add(new IRCResponse(ResponseType.Say, "Maximum bad guesses set to: " + maxBadGuesses, message.ReplyTo));
                         }
                         else
                             responses.Add(new IRCResponse(ResponseType.Say, "Cannot change the maximum bad guesses while a game is in progress!", message.ReplyTo));
                         break;
+                    case "score":
+                        if (message.ParameterList.Count > 1)
+                        {
+                            responses.Add(new IRCResponse(
+                                ResponseType.Say,
+                                GetScore(message.ParameterList[1]),
+                                message.ReplyTo));
+                        }
+                        else
+                        {
+                            responses.Add(new IRCResponse(
+                                ResponseType.Say,
+                                GetScore(message.User.Name),
+                                message.ReplyTo));
+                        }
+                        break;
                     default:
                         if (playing)
                         {
-                            responses.Add(new IRCResponse(ResponseType.Say, Guess(message), message.ReplyTo));
-                            responses.Add(new IRCResponse(ResponseType.Say, VisibleWord(word, guesses), message.ReplyTo));
-                            if (badGuesses == maxBadGuesses)
+                            string guessMsg = Guess(message);
+                            string visibleWord;
+
+                            if (guessMsg != null)
                             {
-                                responses.Add(new IRCResponse(ResponseType.Say, "You have failed! The " + WordOrPhrase(word) + " was: " + word, message.ReplyTo));
+                                responses.Add(new IRCResponse(
+                                    ResponseType.Say,
+                                    guessMsg,
+                                    message.ReplyTo));
+                                visibleWord = VisibleWord(word, guesses);
+                            }
+                            else
+                                visibleWord = word;
+
+                            if (word == visibleWord)
+                            {
+                                if (message.Parameters.ToLowerInvariant().Length == 1)
+                                    IncrementFinalLetter(message.User.Name, 1);
+                                else
+                                    IncrementWord(message.User.Name, 1);
+
+                                responses.Add(new IRCResponse(
+                                    ResponseType.Say,
+                                    "Congratulations " + message.User.Name + "! The " + WordOrPhrase(word) + " was: " + word,
+                                    message.ReplyTo));
                                 Stop();
+                            }
+                            else
+                            {
+                                responses.Add(new IRCResponse(
+                                    ResponseType.Say,
+                                    ProgressString(),
+                                    message.ReplyTo));
+                                if (badGuesses == maxBadGuesses)
+                                {
+                                    responses.Add(new IRCResponse(
+                                        ResponseType.Say,
+                                        "You have failed! The " + WordOrPhrase(word) + " was: " + word,
+                                        message.ReplyTo));
+                                    Stop();
+                                }
                             }
                         }
                         else
                         {
-                            responses.Add(new IRCResponse(ResponseType.Say, "There isn't a Hangman game running right now - use '|hm start' to start one", message.ReplyTo));
+                            responses.Add(new IRCResponse(
+                                ResponseType.Say,
+                                "There isn't a Hangman game running right now - use '|hm start' to start one",
+                                message.ReplyTo));
                         }
                         break;
                 }
@@ -94,6 +157,16 @@ namespace Fun
                 return responses;
             }
             return null;
+        }
+
+        string ProgressString()
+        {
+            return VisibleWord(word, guesses) + " " + WordLength() + " " + BadGuessIndicator() + " " + GuessList();
+        }
+
+        string WordLength()
+        {
+            return "(" + word.Length + ")";
         }
 
         string VisibleWord(string word, List<char> guesses)
@@ -104,46 +177,41 @@ namespace Fun
                 visibleWord += guesses.Contains(c) ? c : (c == ' ' ? ' ' : '_');
             }
 
-            if (word == visibleWord)
-            {
-                Stop();
-                return "Congratulations! The " + WordOrPhrase(visibleWord) + " was: " + visibleWord;
-            }
-
-            string guessString = "";
-            foreach (char guess in guesses)
-                guessString += guess + " ";
-
-            return visibleWord + BadGuessIndicator() + "[ " + guessString + "]";
+            return visibleWord;
         }
 
         string BadGuessIndicator()
         {
-            string indicator = " [";
+            string indicator = "[";
             for (int i = 0; i < maxBadGuesses; ++i)
             {
                 indicator += (i - badGuesses == 0 ? '*' : (i - badGuesses < 0 ? '.' : '-'));
             }
-            return indicator + (badGuesses != maxBadGuesses ? 'O' : '#') + "] ";
+            return indicator + (badGuesses != maxBadGuesses ? 'O' : '#') + "]";
         }
 
-        string Start()
+        string GuessList()
+        {
+            string guessString = "[ ";
+            foreach (char guess in guesses)
+                guessString += guess + " ";
+
+            return guessString + "]";
+        }
+
+        void Start()
         {
             word = words[rand.Next(words.Count)];
             guesses.Clear();
             playing = true;
-
-            return "Hangman game started!";
         }
 
-        string Stop()
+        void Stop()
         {
             word = "";
             guesses.Clear();
             playing = false;
             badGuesses = 0;
-
-            return "Hangman game stopped!";
         }
 
         string Guess(BotMessage message)
@@ -165,16 +233,12 @@ namespace Fun
 
             if (word.Contains(guess.ToString()))
             {
-                SQLiteInterface.Instance.ExecuteNonQuery(
-                    "INSERT OR IGNORE INTO hangman VALUES (" + user.ToLowerInvariant() + ", 0, 0, 0);"+
-                    "UPDATE hangman SET correct = correct + 1 WHERE user LIKE " + user.ToLowerInvariant() + ";");
+                IncrementCorrect(user, 1);
                 return "'" + guess + "' is in the " + WordOrPhrase(word) + "!"; // correct guess
             }
             else
             {
-                SQLiteInterface.Instance.ExecuteNonQuery(
-                    "INSERT OR IGNORE INTO hangman VALUES (" + user.ToLowerInvariant() + ", 0, 0, 0);" +
-                    "UPDATE hangman SET incorrect = incorrect + 1 WHERE user LIKE " + user.ToLowerInvariant() + ";");
+                IncrementIncorrect(user, 1);
                 ++badGuesses;
                 return "'" + guess + "' is not in the " + WordOrPhrase(word); // incorrect guess
             }
@@ -182,12 +246,109 @@ namespace Fun
 
         string GuessWord(string guess, string user)
         {
-            return "Guessing the whole " + WordOrPhrase(word) + " isn't done yet.";
+            if (word == guess)
+            {
+                List<char> unknowns = new List<char>();
+                int correct = 0;
+                foreach (char c in word)
+                {
+                    if (!guesses.Contains(c) && !unknowns.Contains(c))
+                    {
+                        unknowns.Add(c);
+                        ++correct;
+                    }
+                }
+
+                IncrementCorrect(user, correct);
+                return null;
+            }
+            else
+            {
+                IncrementIncorrect(user, 1);
+                return "The " + WordOrPhrase(word) + " is not '" + guess + "'";
+            }
         }
 
         string WordOrPhrase(string stringToTest)
         {
             return (stringToTest.Contains(" ") ? "phrase" : "word");
+        }
+
+        void IncrementCorrect(string user, int amount)
+        {
+            using (SqliteCommand cmd = (SqliteCommand)SQLiteInterface.Instance.Connection.CreateCommand())
+            {
+                cmd.CommandText =
+                    "INSERT OR IGNORE INTO hangman VALUES (@user, 0, 0, 0, 0);" +
+                    "UPDATE hangman SET correct = correct + @amount WHERE user LIKE @user;";
+                cmd.Parameters.Add("@user", user.ToLowerInvariant());
+                cmd.Parameters.Add("@amount", amount);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        void IncrementIncorrect(string user, int amount)
+        {
+            using (SqliteCommand cmd = (SqliteCommand)SQLiteInterface.Instance.Connection.CreateCommand())
+            {
+                cmd.CommandText =
+                    "INSERT OR IGNORE INTO hangman VALUES (@user, 0, 0, 0, 0);" +
+                    "UPDATE hangman SET incorrect = incorrect + @amount WHERE user LIKE @user;";
+                cmd.Parameters.Add("@user", user.ToLowerInvariant());
+                cmd.Parameters.Add("@amount", amount);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        void IncrementWord(string user, int amount)
+        {
+            using (SqliteCommand cmd = (SqliteCommand)SQLiteInterface.Instance.Connection.CreateCommand())
+            {
+                cmd.CommandText =
+                    "INSERT OR IGNORE INTO hangman VALUES (@user, 0, 0, 0, 0);" +
+                    "UPDATE hangman SET word = word + @amount WHERE user LIKE @user;";
+                cmd.Parameters.Add("@user", user.ToLowerInvariant());
+                cmd.Parameters.Add("@amount", amount);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        void IncrementFinalLetter(string user, int amount)
+        {
+            using (SqliteCommand cmd = (SqliteCommand)SQLiteInterface.Instance.Connection.CreateCommand())
+            {
+                cmd.CommandText =
+                    "INSERT OR IGNORE INTO hangman VALUES (@user, 0, 0, 0, 0);" +
+                    "UPDATE hangman SET finalLetter = finalLetter + @amount WHERE user LIKE @user;";
+                cmd.Parameters.Add("@user", user.ToLowerInvariant());
+                cmd.Parameters.Add("@amount", amount);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        string GetScore(string user)
+        {
+            using (SqliteCommand cmd = (SqliteCommand)SQLiteInterface.Instance.Connection.CreateCommand())
+            {
+                cmd.CommandText =
+                    "SELECT correct, incorrect, word, finalLetter FROM hangman WHERE user LIKE @user";
+                cmd.Parameters.Add("@user", user.ToLowerInvariant());
+                SqliteDataReader reader = cmd.ExecuteReader();
+
+                if (!reader.HasRows)
+                    return "No scores held for " + user;
+
+                reader.Read();
+
+                int correct = reader.GetInt32(reader.GetOrdinal("correct"));
+                int incorrect = reader.GetInt32(reader.GetOrdinal("incorrect"));
+                int guessedWord = reader.GetInt32(reader.GetOrdinal("word"));
+                int finalLetter = reader.GetInt32(reader.GetOrdinal("finalLetter"));
+
+                string ratio = (incorrect > 0 ? ((float)correct / (float)incorrect).ToString() : "Infinity!");
+
+                return "Scores for " + user + " - Correct: " + correct + " Incorrect: " + incorrect + " Ratio: " + ratio + " Final Letter: " + finalLetter + " Whole Word: " + guessedWord;
+            }
         }
 
         void LoadWords()
