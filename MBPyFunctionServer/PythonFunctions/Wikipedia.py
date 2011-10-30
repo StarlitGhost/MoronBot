52ece83a5daeb9c2741db27ace8d1d184b822ca6
Matthew Cox <matthewcpcox@gmail.com>
2011-10-30 17:42:50 +0000

    Made Wikipedia.py unescape html character entities.
    Deleted a bunch of really out of date data files.

--- MBPyFunctionServer/PythonFunctions/Wikipedia.py
+++ MBPyFunctionServer/PythonFunctions/Wikipedia.py
@@ -11,6 +11,33 @@ socket.getaddrinfo = getAddrInfoWrapper
 import re
 import urllib, urllib2
 
+import htmlentitydefs
+##
+# Removes HTML or XML character references and entities from a text string.
+#
+# @param text The HTML (or XML) source text.
+# @return The plain text, as a Unicode string, if necessary.
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
+
 class Instantiate(Function):
     Help = "wiki(pedia) <search term> - returns the top result for a given search term from wikipedia."
     
@@ -33,11 +60,12 @@ class Instantiate(Function):
                 content = re.search("\"content\":\"(?P<content>[^\"]+)", data).group('content').decode('unicode-escape')
                 content = re.sub("<.*?>", "", content)
                 content = re.sub("\s+", " ", content)
+                content = unescape(content)
                 url = re.search("\"url\":\"(?P<url>[^\"]+)", data).group('url')
                 replyText = title + " | " + content + " | " + url
                 
                 return IRCResponse(ResponseType.Say, replyText, message.ReplyTo)
-            except:
-                return IRCResponse(ResponseType.Say, "No Results!", message.ReplyTo)
+            except Exception, x:
+                return IRCResponse(ResponseType.Say, x.args, message.ReplyTo)
         
         return
