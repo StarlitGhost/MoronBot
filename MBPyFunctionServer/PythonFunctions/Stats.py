11babe2e163dc076b8e168933badfcee6a22d993
MatthewCox <matthewcpcox@gmail.com>
2012-01-04 00:36:03 +0000

    Added Stats.py, a function which returns a link to the #desertbus chat stats page.

---
+++ MBPyFunctionServer/PythonFunctions/Stats.py
@@ -0,0 +1,27 @@
+'''
+Created on Dec 20, 2011
+
+@author: Tyranic-Moron
+'''
+
+from IRCMessage import IRCMessage
+from IRCResponse import IRCResponse, ResponseType
+from Function import Function
+from GlobalVars import *
+
+import re
+
+class Instantiate(Function):
+    Help = 'Responds with a link to the chat stats webpage'
+
+    def GetResponse(self, message):
+        if not message.MessageString[:1] == '|':
+            return
+        
+        match = re.search('^stats$', message.Command, re.IGNORECASE)
+        if not match:
+            return
+        
+        return IRCResponse(ResponseType.Say,
+                           'http://silver.amazon.fooproject.net/pisg/desertbus.html',
+                           message.ReplyTo)
