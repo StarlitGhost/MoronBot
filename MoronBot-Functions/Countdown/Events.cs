9ef2ab3e73ba4deb89f98465ebdb7fcada419f66
MatthewCox <matthewcpcox@gmail.com>
2012-01-12 01:13:42 +0000

    Changed Event to use DateTimeOffset objects for parsing instead of plain old DateTime objects, per dave_random's request.
    Took the time to invert a bunch of the logic in the various functions under countdown, so they are indented less, and exit conditions are more obvious.
    Also fixed TimeTill and TimeSince to use whatever Desert Bus events are closest, rather than hard-coded DB5 and DB4. All Desert Bus events should be changed to the format 'DB# Started' and 'DB# Ended', however.

--- MoronBot-Functions/Countdown/Events.cs
+++ MoronBot-Functions/Countdown/Events.cs
@@ -9,7 +9,6 @@ using MBUtilities;
 
 namespace Utility
 {
-
     public class Events : Function
     {
         public Events()
@@ -21,53 +20,37 @@ namespace Utility
 
         public override List<IRCResponse> GetResponse(BotMessage message)
         {
-            if (Regex.IsMatch(message.Command, "^(events|upcoming)$", RegexOptions.IgnoreCase))
-            {
-                double daysAhead = 7;
-                if (message.ParameterList.Count > 0)
-                {
-                    if (!Double.TryParse(message.ParameterList[0], out daysAhead))
-                    {
-                        daysAhead = 7;
-                    }
-                    if (Math.Abs(daysAhead) > 36500)
-                    {
-                        daysAhead = 36500;
-                    }
-                    daysAhead = Math.Abs(daysAhead);
-                }
-                List<string> weekEvents = new List<string>();
-                foreach (TimeTill.EventStruct weekEvent in TimeTill.EventList)
-                {
-                    if (weekEvent.EventDate > DateTime.UtcNow && weekEvent.EventDate < DateTime.UtcNow.AddDays(daysAhead))
-                    {
-                        weekEvents.Add(weekEvent.EventName);
-                    }
-                }
-
-                if (weekEvents.Count > 0)
-                {
-                    string events = "";
-                    foreach (string weekEvent in weekEvents)
-                    {
-                        events += weekEvent + ", ";
-                    }
-                    events = events.Remove(events.Length - 2);
-
-                    List<IRCResponse> responses = new List<IRCResponse>();
-                    responses.Add(new IRCResponse(ResponseType.Say, "Events in the next " + daysAhead + " days:", message.ReplyTo));
-                    responses.Add(new IRCResponse(ResponseType.Say, events, message.ReplyTo));
-                    return responses;
-                }
-                else
-                {
-                    return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "There are no events in the coming " + daysAhead + " days!", message.ReplyTo) };
-                }
-            }
-            else
-            {
+            if (!Regex.IsMatch(message.Command, "^(events|upcoming)$", RegexOptions.IgnoreCase))
                 return null;
+
+            double daysAhead = 7;
+            if (message.ParameterList.Count > 0)
+            {
+                if (!Double.TryParse(message.ParameterList[0], out daysAhead))
+                    daysAhead = 7;
+                if (Math.Abs(daysAhead) > 36500)
+                    daysAhead = 36500;
+                daysAhead = Math.Abs(daysAhead);
             }
+            List<string> weekEvents = new List<string>();
+            foreach (TimeTill.EventStruct weekEvent in TimeTill.EventList)
+            {
+                if (weekEvent.EventDate > DateTime.UtcNow &&
+                    weekEvent.EventDate < DateTime.UtcNow.AddDays(daysAhead))
+                {
+                    weekEvents.Add(weekEvent.EventName);
+                }
+            }
+
+            if (weekEvents.Count == 0)
+                return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "There are no events in the coming " + daysAhead + " days!", message.ReplyTo) };
+                    
+            string events = String.Join(" | ", weekEvents);
+
+            List<IRCResponse> responses = new List<IRCResponse>();
+            responses.Add(new IRCResponse(ResponseType.Say, "Events in the next " + daysAhead + " days:", message.ReplyTo));
+            responses.Add(new IRCResponse(ResponseType.Say, events, message.ReplyTo));
+            return responses;
         }
     }
 }
--- MoronBot-Functions/Countdown/TimeSince.cs
+++ MoronBot-Functions/Countdown/TimeSince.cs
@@ -22,47 +22,43 @@ namespace Utility
 
         public override List<IRCResponse> GetResponse(BotMessage message)
         {
-            if (Regex.IsMatch(message.Command, "^(timesince|whenwas)$", RegexOptions.IgnoreCase))
-            {
-                TimeTill.EventStruct eventStruct;
-                TimeSpan timeSpan;
-
-                List<TimeTill.EventStruct> reversedList = new List<TimeTill.EventStruct>(TimeTill.EventList);
-                reversedList.Reverse();
-
-                if (message.ParameterList.Count > 0) // Parameters given
-                {
-                    // If an event in EventList is in the past, and the event name and command parameters match exactly,
-                    // assign to eventStruct
-                    eventStruct = reversedList.Find(s =>
-                       s.EventDate <= DateTime.UtcNow &&
-                       s.EventName == message.Parameters);
-
-                    // If no matching message found
-                    if (eventStruct.EventName == null)
-                    {
-                        // Same search as above, but using regex to match messages this time.
-                        eventStruct = reversedList.Find(s =>
-                            s.EventDate <= DateTime.UtcNow &&
-                            Regex.IsMatch(s.EventName, ".*" + message.Parameters + ".*", RegexOptions.IgnoreCase));
-                    }
-
-                    // No matching events found
-                    if (eventStruct.EventName == null)
-                        return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "No event matching \"" + message.Parameters + "\" found in the past events list!", message.ReplyTo) };
-                }
-                else // Parameters not given, assign last Desert Bus to eventStruct.
-                {
-                    eventStruct = reversedList.Find(s => s.EventName == "DB4 Ended");
-                }
-
-                timeSpan = DateTime.UtcNow - eventStruct.EventDate;
-                return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, eventStruct.EventName + " occurred " + timeSpan.Days + " day(s) " + timeSpan.Hours + " hour(s) " + timeSpan.Minutes + " minute(s) ago", message.ReplyTo) };
-            }
-            else
-            {
+            if (!Regex.IsMatch(message.Command, "^(timesince|whenwas)$", RegexOptions.IgnoreCase))
                 return null;
+
+            TimeTill.EventStruct eventStruct;
+            TimeSpan timeSpan;
+
+            List<TimeTill.EventStruct> reversedList = new List<TimeTill.EventStruct>(TimeTill.EventList);
+            reversedList.Reverse();
+
+            if (message.ParameterList.Count > 0) // Parameters given
+            {
+                // If an event in EventList is in the past, and the event name and command parameters match exactly,
+                // assign to eventStruct
+                eventStruct = reversedList.Find(s =>
+                    s.EventDate <= DateTime.UtcNow &&
+                    s.EventName == message.Parameters);
+
+                // If no matching message found
+                if (eventStruct.EventName == null)
+                {
+                    // Same search as above, but using regex to match messages this time.
+                    eventStruct = reversedList.Find(s =>
+                        s.EventDate <= DateTime.UtcNow &&
+                        Regex.IsMatch(s.EventName, ".*" + message.Parameters + ".*", RegexOptions.IgnoreCase));
+                }
+
+                // No matching events found
+                if (eventStruct.EventName == null)
+                    return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "No event matching \"" + message.Parameters + "\" found in the past events list!", message.ReplyTo) };
             }
+            else // Parameters not given, assign last Desert Bus to eventStruct.
+            {
+                eventStruct = reversedList.Find(s => s.EventName.StartsWith("DB"));
+            }
+
+            timeSpan = DateTime.UtcNow - eventStruct.EventDate;
+            return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, eventStruct.EventName + " occurred " + timeSpan.Days + " day(s) " + timeSpan.Hours + " hour(s) " + timeSpan.Minutes + " minute(s) ago", message.ReplyTo) };
         }
     }
 }
