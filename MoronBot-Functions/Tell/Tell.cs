76820756a46d11f406625df7b5c408c534e60e8d
MatthewCox <matthewcpcox@gmail.com>
2012-01-14 22:55:19 +0000

    Added 'message to bot' detection to Tell.
    Also rewrote it a bit to be less awful (mostly just code layout).

--- MoronBot-Functions/Tell/Tell.cs
+++ MoronBot-Functions/Tell/Tell.cs
@@ -50,65 +50,35 @@ namespace Utility
 
         public override List<IRCResponse> GetResponse(BotMessage message)
         {
-            if (Regex.IsMatch(message.Command, "^(tell)$", RegexOptions.IgnoreCase))
-            {
-                int i = userDateNumberList.FindIndex(s => s.User == message.User.Name);
+            if (!Regex.IsMatch(message.Command, "^(tell)$", RegexOptions.IgnoreCase))
+                return null;
 
-                if (i < 0)
-                {
-                    userDateNumberList.Add(new UserDateNumber() { User = message.User.Name, MessageTime = DateTime.Now, Number = 0 });
-                    i = userDateNumberList.FindIndex(s => s.User == message.User.Name);
-                }
-
-                UserDateNumber userDateNumber = userDateNumberList[i];
-
-                if (userDateNumber.MessageTime.AddMinutes(5).CompareTo(DateTime.Now) <= 0)
-                {
-                    userDateNumber.MessageTime = DateTime.Now;
-                    userDateNumber.Number = 0;
-                }
-                else
-                {
-                    if (userDateNumber.Number < 3)
-                    {
-                        userDateNumber.MessageTime = DateTime.Now;
-                        userDateNumber.Number++;
-                        userDateNumberList[i] = userDateNumber;
-                    }
-                    else
-                    {
-                        return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "You've already sent 3 messages in the last 5 minutes, slow down!", message.ReplyTo) };
-                    }
-                }
-
-                if (message.ParameterList.Count > 1)
-                {
-                    string to = WildcardToRegex(message.ParameterList[0]);
-                    string msg = message.Parameters.Substring(message.ParameterList[0].Length + 1);
-                    if (msg.Trim(' ').Length > 0)
-                    {
-                        if (!MessageMap.ContainsKey(to))
-                        {
-                            MessageMap.Add(to, new List<TellMessage>());
-                        }
-                        TellMessage tellMessage = new TellMessage();
-                        tellMessage.From = message.User.Name;
-                        tellMessage.SentDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss (UTC zz)");
-                        tellMessage.Message = StringUtils.ReplaceNewlines(StringUtils.StripIRCFormatChars(msg), " | ");
-                        tellMessage.Target = message.TargetType == 0 ? message.ReplyTo : "PM";
-                        MessageMap[to].Add(tellMessage);
-                        WriteMessages();
-                        return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "Ok, I'll tell " + message.ParameterList[0] + " that when they next speak.", message.ReplyTo) };
-                    }
-                }
-
-                if (message.ParameterList.Count > 0)
-                {
-                    return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "You didn't give a message for me to tell " + message.ParameterList[0], message.ReplyTo) };
-                }
+            if (message.ParameterList.Count == 0)
                 return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "Tell who what?", message.ReplyTo) };
+
+            if (message.ParameterList.Count == 1 || message.Parameters.Substring(message.ParameterList[0].Length + 1).Trim(' ').Length == 0)
+                return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "You didn't give a message for me to tell " + message.ParameterList[0], message.ReplyTo) };
+
+            if (message.ParameterList[0] == Settings.Instance.CurrentNick)
+                return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "Thanks for telling me that " + message.User.Name + ", I'm sure I'll find that useful...", message.ReplyTo) };
+
+            if (RateLimit(message.User.Name))
+                return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "You've already sent 3 messages in the last 5 minutes, slow down!", message.ReplyTo) };
+
+            string to = WildcardToRegex(message.ParameterList[0]);
+            string msg = message.Parameters.Substring(message.ParameterList[0].Length + 1);
+            if (!MessageMap.ContainsKey(to))
+            {
+                MessageMap.Add(to, new List<TellMessage>());
             }
-            return null;
+            TellMessage tellMessage = new TellMessage();
+            tellMessage.From = message.User.Name;
+            tellMessage.SentDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss (UTC zz)");
+            tellMessage.Message = StringUtils.ReplaceNewlines(StringUtils.StripIRCFormatChars(msg), " | ");
+            tellMessage.Target = message.TargetType == 0 ? message.ReplyTo : "PM";
+            MessageMap[to].Add(tellMessage);
+            WriteMessages();
+            return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "Ok, I'll tell " + message.ParameterList[0] + " that when they next speak.", message.ReplyTo) };
         }
 
         public static void WriteMessages()
@@ -192,6 +162,32 @@ namespace Utility
                 Replace("\\(", "(").
                 Replace("\\)", ")").
                 Replace("/", "|") + "$";
+        }
+
+        bool RateLimit(string userName)
+        {
+            int i = userDateNumberList.FindIndex(s => s.User == userName);
+            if (i < 0)
+            {
+                userDateNumberList.Add(new UserDateNumber() { User = userName, MessageTime = DateTime.Now, Number = 0 });
+                i = userDateNumberList.FindIndex(s => s.User == userName);
+            }
+            UserDateNumber userDateNumber = userDateNumberList[i];
+            if (userDateNumber.MessageTime.AddMinutes(5).CompareTo(DateTime.Now) <= 0)
+            {
+                userDateNumber.MessageTime = DateTime.Now;
+                userDateNumber.Number = 0;
+            }
+            else
+            {
+                if (userDateNumber.Number >= 3)
+                    return false;
+
+                userDateNumber.MessageTime = DateTime.Now;
+                userDateNumber.Number++;
+                userDateNumberList[i] = userDateNumber;
+            }
+            return true;
         }
     }
 }
