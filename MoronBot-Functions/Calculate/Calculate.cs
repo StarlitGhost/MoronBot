7c729a94be522817d030cf374fa91e70071069af
MatthewCox <matthewcpcox@gmail.com>
2012-01-21 22:55:35 +0000

    Rewrote Calculate to use Google's unofficial calculator API.

--- MoronBot-Functions/Calculate/Calculate.cs
+++ MoronBot-Functions/Calculate/Calculate.cs
@@ -1,190 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
 using System.Text.RegularExpressions;
 
 using CwIRC;
 using MBFunctionInterface;
+using MBUtilities;
 
-using MBUtilities.Calc;
+using Newtonsoft.Json;
+using System.Web;
 
 namespace Utility
 {
     public class Calculate : Function
     {
-        List<Operator> Operators { get; set; }
+        //http://www.google.com/ig/calculator?hl=en&q=sin(3)USD+in+GBP
+        //{lhs: "sin(3) * U.S. dollar",rhs: "0.0908049727 British pounds",error: "",icc: true}
 
         public Calculate()
         {
-            Help = "calc <expr> - Calculates the result of the given expression. " +
-                "Supported operations, in decreasing order of precedence, are: ( ), ^, % / *, - +";
+            Help = "calc <expr> - Uses Google's calculation API to give you the result of <expr>";
             Type = Types.Command;
             AccessLevel = AccessLevels.Anyone;
-
-            Operators = PopulateOperators();
         }
 
         public override List<IRCResponse> GetResponse(BotMessage message)
         {
-            if (Regex.IsMatch(message.Command, @"^(calc(ulate)?)$", RegexOptions.IgnoreCase))
+            if (!Regex.IsMatch(message.Command, @"^(calc(ulate)?)$", RegexOptions.IgnoreCase))
+                return null;
+            if (message.ParameterList.Count == 0)
+                return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "You didn't give an expression to calculate!", message.ReplyTo) };
+
+            try
             {
-                if (message.ParameterList.Count > 0) // Expression given
-                {
-                    AllOperators.Operators = Operators;
+                string query = HttpUtility.UrlEncode(message.Parameters);
+                Stream responseStream = SendToServer("http://www.google.com/ig/calculator?hl=en&q=" + query);
+                string jsonResponse = ReceiveFromServer(responseStream);
 
-                    string stringExpr = Regex.Replace(message.Parameters, @"\w+", MathsConstants);
+                Result result = JsonConvert.DeserializeObject<Result>(jsonResponse);
+                if (result.rhs.Length > 0)
+                    return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "Result: " + result.rhs, message.ReplyTo) };
+                if (result.error.Length > 0)
+                    return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "Error: " + result.error, message.ReplyTo) };
 
-                    Expression expr = Tokenizer.Split(stringExpr);
-
-                    List<Token> postfixTokens = new List<Token>();
-                    bool isError = false;
-                    int errorTokenIndex = -1;
-                    string errorMessage = string.Empty;
-
-                    Evaluator.Validate(expr, ref postfixTokens, out isError, out errorTokenIndex, out errorMessage);
-
-                    string output = "";
-
-                    if (CalcUtilities.IsArithmeticExpression(expr))
-                    {
-                        if (!isError)
-                        {
-                            double result = Evaluator.EvaluateBasic(postfixTokens);
-                            output = "Result: " + result.ToString();
-                        }
-                        else
-                        {
-                            output = "Error at Token " + (errorTokenIndex + 1) + " ['" + expr.Tokens[errorTokenIndex].LinearToken + "']. " + errorMessage;
-                        }
-                    }
-                    else
-                    {
-                        output = "Expression is not arithmetic.";
-                    }
-
-                    return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, output, message.ReplyTo) };
-                }
-                else // No expr given
-                {
-                    return new List<IRCResponse>() { new IRCResponse(ResponseType.Say, "You didn't give an expression to calculate!", message.ReplyTo) };
-                }
+                return null;
             }
-            else
+            catch (System.Exception ex)
             {
+                Logger.Write(ex.Message, Settings.Instance.ErrorFile);
                 return null;
             }
         }
 
-        static string MathsConstants(Match match)
+        Stream SendToServer(string url)
         {
-            switch (match.Value)
-            {
-                case "pi":
-                    return "3.14159265358979323846264338327950288419716939937510";
-                case "e":
-                    return "2.71828182845904523536028747135266249775724709369995";
-                case "g":
-                    return "9.80665";
-                case "G":
-                    return Math.Pow(6.67428, 11).ToString();
-                case "c":
-                    return "299792458";
-            }
-
-            return match.Value;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse webResponse = (HttpWebResponse)request.GetResponse();
+            return webResponse.GetResponseStream();
         }
 
-        static List<Operator> PopulateOperators()
+        string ReceiveFromServer(Stream responseStream)
         {
-            List<Operator> opList = new List<Operator>();
+            Encoding encode = Encoding.UTF8;
+            StreamReader stream = new StreamReader(responseStream, encode);
 
-            opList.Add(new Operator()
-            {
-                SymbolText = "+",
-                OperandCount = 2,
-                PrecedenceLevel = 5,
-                Associativity = OperatorAssociativity.LeftToRight,
-                Execute = (x, y) => x + y
-            });
+            StringBuilder sb = new StringBuilder();
+            sb.Append(stream.ReadToEnd());
+            return sb.ToString();
+        }
 
-            opList.Add(new Operator()
-            {
-                SymbolText = "-",
-                OperandCount = 2,
-                PrecedenceLevel = 5,
-                Associativity = OperatorAssociativity.LeftToRight,
-                Execute = (x, y) => x - y
-            });
-
-            opList.Add(new Operator()
-            {
-                SymbolText = "*",
-                OperandCount = 2,
-                PrecedenceLevel = 4,
-                Associativity = OperatorAssociativity.LeftToRight,
-                Execute = (x, y) => x * y
-            });
-
-            opList.Add(new Operator()
-            {
-                SymbolText = "/",
-                OperandCount = 2,
-                PrecedenceLevel = 4,
-                Associativity = OperatorAssociativity.LeftToRight,
-                Execute = (x, y) => x / y
-            });
-
-            opList.Add(new Operator()
-            {
-                SymbolText = "%",
-                OperandCount = 2,
-                PrecedenceLevel = 4,
-                Associativity = OperatorAssociativity.LeftToRight,
-                Execute = (x, y) => x % y
-            });
-
-            opList.Add(new Operator()
-            {
-                SymbolText = "^",
-                OperandCount = 2,
-                PrecedenceLevel = 3,
-                Associativity = OperatorAssociativity.LeftToRight,
-                Execute = (x, y) => Math.Pow(x, y)
-            });
-
-            opList.Add(new Operator()
-            {
-                SymbolText = "_",
-                OperandCount = 1,
-                PrecedenceLevel = 2,
-                Associativity = OperatorAssociativity.RightToLeft,
-                Execute = (x, y) => -x
-            });
-
-            opList.Add(new Operator()
-            {
-                SymbolText = "(",
-                OperandCount = 0,
-                PrecedenceLevel = 1,
-                Associativity = OperatorAssociativity.LeftToRight
-            });
-
-            opList.Add(new Operator()
-            {
-                SymbolText = ")",
-                OperandCount = 0,
-                PrecedenceLevel = 1,
-                Associativity = OperatorAssociativity.LeftToRight
-            });
-
-            opList.Add(new Operator()
-            {
-                SymbolText = "X",
-                OperandCount = 0,
-                PrecedenceLevel = 0,
-                Associativity = OperatorAssociativity.LeftToRight
-            });
-
-            return opList;
+        class Result
+        {
+            //{lhs: "sin(3) * U.S. dollar",rhs: "0.0908049727 British pounds",error: "",icc: true}
+            public string lhs;
+            public string rhs;
+            public string error;
+            public string icc;
         }
     }
 }
